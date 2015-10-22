8db00d012428f3c1af1361d4b572bf030c00944b
Marcus Barnes <mavjbarnes@gmail.com>
2015-10-22 00:13:50 -0400

    refactor! using promises

--- server.js
+++ server.js
@@ -13,41 +13,65 @@ const contentTypes = {
   '.jpg': 'image/jpeg'
 }
 
+/***************************************************************
+** Using Promises to break up the fs access & readFile methods
+****************************************************************/
+
+let fileAccess = (filepath) => {
+  return new Promise((resolve, reject) => {
+    fs.access(filepath, fs.F_OK, (error) => {
+      if (!error) {
+        resolve(filepath);
+      } else {
+        reject(error);
+      }
+    });
+  });
+};
+
+let fileReader = (filepath) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filepath, (error, content) => {
+      if (!error) {
+        resolve(content);
+      } else {
+        reject(error);
+      }
+    });
+  });
+};
+
 let staticServer = (req, res) => {
 
   // Getting the URL {}
   let baseURI = url.parse(req.url);
+
   // Getting the file path
   // if route reqest is '/', then load 'index.html'
   // else use whatever route request is given
   let filePath = __dirname + (baseURI.pathname === '/' ?  '/index.html' : baseURI.pathname);
-  console.log(filePath);
-  console.log(req.url);
 
-  // Checking if the file is accessable by checking if the filepath is available
-  // callback checks for errors
-  fs.access(filePath, fs.F_OK, (error) => {
-    if(!error) {
-      // Read and Serve files
-      fs.readFile(filePath, (error, content) => {
-        if (!error) {
-          // getting the content-type by using file extension from file
-          let contentType = contentTypes[path.extname(filePath)];
+  // getting the content-type by using file extension from file
+  let contentType = contentTypes[path.extname(filePath)];
 
-          // Serving the file with the correct content-type
-          res.writeHead(200, {'Content-type': contentType});
-          res.end(content, 'utf-8');
-        } else {
-          res.writeHead(500);
-          res.end('Server could not read the file')
-        }
-      });
-    } else {
+  console.log('Requst URL: ', req.url);
+  console.log('File Extension: ', path.extname(filePath))
+  console.log('Content Type: ', contentType);
+  console.log('File Location: ', filePath);
+  console.log('\n');
+
+  // invoke the fileAccess() with the filePath var to start the Promise
+  fileAccess(filePath) // if resolve invoke fileReader & pass filePath
+    .then(fileReader) // if resolve return the content
+    .then((content) => {
+      res.writeHead(200, {'Content-type': contentType});
+      res.end(content, 'utf-8');
+    })
+    .catch((error) => {
       res.writeHead(404);
-      res.end('Content DNE!');
-    }
-  });
-}
+      res.end(JSON.stringify(error));
+    });
+};
 
 // Server
 http.createServer(staticServer).listen(3000, () => {
